8918d756e0d77da52c13971ff54adc6d65e13217
lota02 <ogbonnalotachukwu02@yahoo.com>
2022-03-29 15:35:07 +0200

    First commit

---
+++ views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(response):
+    return render(response,"prototype/home.html",{})
+
+def about(response):
+    return render(response,"prototype/about.html",{})
+
+def feedback(response):
+    return render(response,"prototype/feedback.html",{})
+
+def contact(response):
+    return render(response,"prototype/contact.html",{})
